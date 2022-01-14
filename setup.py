66c81a5c1a9858d4baab66b17dcd3c7da6a29811
sugam0301 <sugammehta0301@gmail.com>
2022-01-14 22:35:13 +0530

    first commit in mlops

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying MLOps on the Machine Learning models using tools such as DVC, ML Flow, Heroku, EvidentAI, Github Actions and Circle CI',
+    author='Sugam Mehta',
+    license='MIT',
+)
